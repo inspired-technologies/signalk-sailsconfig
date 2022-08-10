f8ec0ec3acd98d48bf4926be1ae4ccbe1cc519d4
Inspired <service@inspiredtechnologies.eu>
2022-08-10 12:24:53 +0200

    Initial from Fork

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Sails configuration and inventory
 Signal K Node server plugin to configure a vessel's sails inventory and current configuration.
 
-![image](https://user-images.githubusercontent.com/3346/207840652-7acd5ecd-7933-4ce9-b0c0-29f8039eb843.png)
+![image](https://user-images.githubusercontent.com/1049678/27000662-dec36db8-4dbf-11e7-93ab-fb2accbcd9e5.png)
--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 /*
- * Copyright 2017 Teppo Kurki <teppo.kurki@iki.fi>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+    Copyright © 2026 Inspired Technologies GmbH (www.inspiredtechnologies.eu)
+    forked from @signalk/sailsconfiguration by 2017 Teppo Kurki <teppo.kurki@iki.fi>
+    License granted under the Apache License, Version 2.0 (the "License")
+ 
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+*/
 const openAPI = require('./openapi.json');
 
-const pluginId = "sailsconfiguration";
+const pluginId = "signalk-sailsconfig";
 
 module.exports = function(app) {
   let plugin = {};
@@ -92,7 +93,7 @@ module.exports = function(app) {
   plugin.id = pluginId;
   plugin.name = "Sails Configuration";
   plugin.description =
-    "Plugin that allows you to define your vessel's sails inventory and configuration";
+    "Plugin to define and manage your vessel's sails inventory and configuration";
 
   plugin.schema = {
     type: "object",
@@ -117,9 +118,20 @@ module.exports = function(app) {
             },
             name: {
               type: "string",
-              title: "Name"
+              title: "Name or Label",
+              description: "An unique identifier by which the crew identifies a sail"
             },
-            description: {
+            name: {
+              type: "bool",
+              title: "Name or Label",
+              description: "An unique identifier by which the crew identifies a sail"
+            },
+            material: {
+              type: "string",
+              title: "Material",
+              description: "[Optional] The material the sail is made from"
+            },
+            brand: {
               type: "string",
               title: "Description",
               'ui:widget': 'textarea',
